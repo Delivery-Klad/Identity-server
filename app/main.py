72f536b81737c6554747613eaaff78a07fa9a5e0
Danila <dakfadeev@gmail.com>
2022-07-05 00:58:35 +0300

    return credentials

--- app/main.py
+++ app/main.py
@@ -21,7 +21,7 @@ app.include_router(auth.router)
 app.add_middleware(
     CORSMiddleware,
     allow_origins=["http://localhost", "https://w3bt.herokuapp.com"],
-    allow_credentials=False,
+    allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
 )
